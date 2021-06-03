bd5f35637db84ad0ba3205f9a9866aebcc6f084f
nguyen khanh <85268443+nguyenkhanh2929@users.noreply.github.com>
2021-06-03 11:02:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     introTitle: 'em à!',
-    introDesc: `Người ta đồn tình yêu trong như gọt nước
-    cho anh gửi chút nước vào bờ môi em`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều lắm lắm luôn í 😘😘',
-    messDesc: 'Tối nay 8h chỗ cũ nhé công chúa.',
-    btnAccept: 'Okiiiii anh iuuuuuu <3',
+    introDesc: `Dạo này không có thính để thả 
+                Mong em tự ngả vào lòng anh `,
+    btnIntro: 'anh thả thính để tán em á',
+    title: ' Người ta đồn tình yêu trong như gọt nước cho anh gửi chút nước vào bờ môi em  😙',
+    desc: 'hôm nay ăn mặc chỉnh tề , để lừa ai đó tối về măm măm ',
+    btnYes: 'Uầy,yêu anh lắm luôn í <33',
+    btnNo: 'Không, Anh ơi em có người yêu rồi anh ạ :3',
+    question:'Tại sao em lại yêu anh ? <3',
+    btnReply: 'Send My Love <3',
+    reply: 'em cũng không biết tại sao lại yêu anh nữa nhưng mà kệ đi em vẫn yêu anh hahha <33333333',
+    mess: 'Anh điều đấy mà 🥰. Yêu em nhiều nhiều lắm lắm luôn í 😘😘',
+    messDesc: 'Tối nay 8h anh qua chở em đi uống bia nhé công chúa.',
+    btnAccept: 'Okiiiii anh iuuuu tối nay uống bia hơi nhé!!! <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
