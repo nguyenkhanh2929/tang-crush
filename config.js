567b84da38f510dd614004c2b6fb2b02aa071d75
nguyen khanh <85268443+nguyenkhanh2929@users.noreply.github.com>
2021-06-03 11:09:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'em à!',
+    introTitle: 'Này cô công chúa à!',
     introDesc: `Dạo này không có thính để thả 
                 Mong em tự ngả vào lòng anh `,
     btnIntro: 'anh thả thính để tán em á',
@@ -7,10 +7,10 @@ const CONFIG = {
     desc: 'hôm nay ăn mặc chỉnh tề , để lừa ai đó tối về măm măm ',
     btnYes: 'Uầy,yêu anh lắm luôn í <33',
     btnNo: 'Không, Anh ơi em có người yêu rồi anh ạ :3',
-    question:'Tại sao em lại yêu anh ? <3',
+    question:'Tại sao em lại đem lòng yêu một người như anh ? <3',
     btnReply: 'Send My Love <3',
-    reply: 'em cũng không biết tại sao lại yêu anh nữa nhưng mà kệ đi em vẫn yêu anh hahha <33333333',
-    mess: 'Anh điều đấy mà 🥰. Yêu em nhiều nhiều lắm lắm luôn í 😘😘',
+    reply: 'em cũng không biết tại sao? nhưng mà yêu thì đâu cần lí do đâu anh hahha <33333333',
+    mess: 'Anh biết điều đấy mà 🥰. Anh nhớ em lắm  í 😘😘',
     messDesc: 'Tối nay 8h anh qua chở em đi uống bia nhé công chúa.',
     btnAccept: 'Okiiiii anh iuuuu tối nay uống bia hơi nhé!!! <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
