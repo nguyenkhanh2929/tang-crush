26f2972f0d483d0d2eba857cef50b14ffd6c0c7a
nguyen khanh <85268443+nguyenkhanh2929@users.noreply.github.com>
2021-06-03 10:38:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
 const CONFIG = {
     introTitle: 'em à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Người ta đồn tình yêu trong như gọt nước
+    cho anh gửi chút nước vào bờ môi em`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
@@ -12,7 +10,7 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều lắm lắm luôn í 😘😘',
     messDesc: 'Tối nay 8h chỗ cũ nhé công chúa.',
     btnAccept: 'Okiiiii anh iuuuuuu <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
